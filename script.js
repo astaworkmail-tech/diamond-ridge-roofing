058b1839b971b9b762c7ae79e985c9226c6a9507
astaworkmail-tech <astaworkmail@gmail.com>
2026-05-29 12:53:29 +1200

    Deploy landing page: Diamond Ridge Roofing

---
+++ script.js
@@ -0,0 +1,2 @@
+const io=new IntersectionObserver((es)=>{es.forEach(e=>{if(e.isIntersecting){e.target.classList.add('in');io.unobserve(e.target);}})},{threshold:.12});
+  document.querySelectorAll('[data-rise]').forEach((el,i)=>{el.style.transitionDelay=(i%4*60)+'ms';io.observe(el);});
